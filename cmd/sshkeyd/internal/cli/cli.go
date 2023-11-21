34cd7e0d445b14bd67878df7a8de51b3a32cd5be
Eric Marshall <ericmarshall715@gmail.com>
2023-11-20 21:19:41 -0500

    Support actually running the sync as a daemon

--- cmd/sshkeyd/internal/cli/cli.go
+++ cmd/sshkeyd/internal/cli/cli.go
@@ -1,8 +1,11 @@
 package cli
 
+import "time"
+
 type CommonFlags struct {
-	GithubUsers        []string `name:"github"`
-	GitlabUsers        []string `name:"gitlab"`
-	Urls               []string `name:"url"`
-	AuthorizedKeysFile string   `name:"keys-file" short:"f" required:"" default:"$HOME/.ssh/authorized_keys"`
+	GithubUsers        []string      `name:"github"`
+	GitlabUsers        []string      `name:"gitlab"`
+	Urls               []string      `name:"url"`
+	AuthorizedKeysFile string        `name:"keys-file" short:"f" required:"" default:"$HOME/.ssh/authorized_keys"`
+	RefreshInterval    time.Duration `name:"refresh-interval" short:"i" default:"0m"`
 }
--- cmd/sshkeyd/internal/cli/sync.go
+++ cmd/sshkeyd/internal/cli/sync.go
@@ -5,14 +5,29 @@ import (
 	"os"
 
 	"github.com/egoodhall/tools/cmd/sshkeyd/internal/sync"
+	"github.com/egoodhall/tools/pkg/daemon"
+	"github.com/kardianos/service"
 )
 
 type SyncCmd struct {
 	Flags CommonFlags `embed:""`
-	Write bool        `name:"write" short:"w" default:"true"`
+	Write bool        `name:"write" short:"w" default:"true" negatable:""`
 }
 
 func (cmd *SyncCmd) Run() error {
+	if cmd.Flags.RefreshInterval == 0 {
+		return cmd.run(nil)
+	}
+
+	dmn, err := daemon.New("sshkeyd", "", daemon.Periodic(cmd.Flags.RefreshInterval, cmd.run))
+	if err != nil {
+		return err
+	}
+
+	return dmn.Run()
+}
+
+func (cmd *SyncCmd) run(logger service.Logger) error {
 	keys, err := sync.Run(sync.Config{
 		AuthorizedKeysFile: os.ExpandEnv(cmd.Flags.AuthorizedKeysFile),
 		RemoteKeyUrls:      sync.GetUrls(cmd.Flags.GithubUsers, cmd.Flags.GitlabUsers, cmd.Flags.Urls),
