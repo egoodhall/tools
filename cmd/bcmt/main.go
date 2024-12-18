836f8449ea0c0913a8e40a1f1c07cc746b82840f
Eric Goodhall <egoodhall@hubspot.com>
2024-12-18 10:45:58 -0500

    Add indent level

--- cmd/bcmt/main.go
+++ cmd/bcmt/main.go
@@ -15,10 +15,12 @@ const template = `
 func main() {
 	var (
 		comment string
+		indent  int
 		help    bool
 	)
 
 	flag.BoolVar(&help, "h", false, "Show this help message")
+	flag.IntVar(&indent, "i", 2, "Indentation level")
 	flag.Parse()
 	comment = strings.Join(flag.Args(), " ")
 
@@ -29,6 +31,15 @@ func main() {
 
 	comment = strings.TrimSpace(comment)
 	pipes := strings.Repeat("-", len(comment))
+	tmpl := strings.TrimLeft(template, "\n")
 
-	fmt.Printf(strings.TrimSpace(template)+"\n", pipes, comment, pipes)
+	fmt.Printf(indentString(tmpl, indent), pipes, comment, pipes)
+}
+
+func indentString(str string, n int) string {
+	lines := strings.Split(str, "\n")
+	for i, line := range lines {
+		lines[i] = strings.Repeat(" ", n) + line
+	}
+	return strings.Join(lines, "\n")
 }
